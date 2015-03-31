7492bab57c603d6cba16cb3717cffe2e47755484
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-03-31 15:49:10 +0200

    release date fixed

--- web/include/news.php
+++ web/include/news.php
@@ -1,5 +1,5 @@
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
-  <?php add_dateitem("April 1, 2015", 'Web site online.') ?>
+  <?php add_dateitem("March 31, 2015", 'Web site online.') ?>
   <li class="newslibtn" data-icon="plus" data-theme="a" onclick="showall();"><a>Older News</a></li>
 </ul>
 
