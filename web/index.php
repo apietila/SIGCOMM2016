e6d7f95dfefaaf401f99b09d0969b8f6bd8efdc9
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-06-05 17:20:45 +0200

    conf dates

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 
 <h1>Welcome to Salvador</h1>
 
-<p>The organizing committee is delighted to invite you to ACM SIGCOMM 2016, to be held in Salvador, Brazil, in August, 2016.</p>
+<p>The organizing committee is delighted to invite you to ACM SIGCOMM 2016, to be held in Salvador, Brazil, in August 22-26, 2016.</p>
 
 <p>SIGCOMM is the flagship annual conference of the ACM Special Interest Group on Data Communication (SIGCOMM) on the applications, technologies, architectures, and protocols for computer communication.</p>
 
