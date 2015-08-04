e983be2e47e580d81bb8bfa614aabb8480434629
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-08-04 10:42:52 +0200

    more local info

---
+++ web/hotel.php
@@ -0,0 +1,43 @@
+<?php
+    $page_title="Hotels and Travel";
+    include("include/header.php");
+?>
+
+<h1>Hotels and Travel</h1>
+
+<h2>Hotels</h2>
+
+The main conference venue is <a href="http://www.othon.com.br/en/hoteis/hotel-em-salvador/bahia-othon-palace#o-hotel">Bahia Othon Palace</a> hotel. 
+More information about reservation and alternative hotels coming up later.
+
+<h2>Travel</h2>
+
+<h3>Airport</h3>
+
+<p>
+<a href="http://www.aeroportosalvador.net/en/">The Salvador's Deputado Luis Eduardo Magalh&#227;es International Airport</a> (IATA: SSA, ICAO: SBSV) 
+is one of Brazil's main airports. All of the biggest Brazilian airlines have flights to the Bahian capital city. The city also receives flights 
+from the main hubs of Europe, South America and the United States. Scheduled Airlines:
+<ul>
+    <li>TAM (Domestic flights)</li>
+    <li>Gol (Domestic flights and Buenos Aires)</li>
+    <li>Avianca (Domestic flights)</li>
+    <li>Azul Linhas A&#233;reas (Domestic and regional flights)</li>
+    <li>Passaredo (Regional flights)</li>
+    <li>TAP (Lisbon)</li>
+    <li>American Airlines (Miami)</li>
+    <li>Condor (Frankfurt)</li>
+    <li>Aerolineas Argentinas (Buenos Aires)</li>
+    <li>Air Europa(Madrid)</li>
+</ul>
+</p>
+
+<h3>From Airport to Conference Venue</h3>
+
+<p>Read about travel options at <a href="http://wikitravel.org/en/Salvador">WikiTravel</a>. The conference venue is located about 25km away from the airport (see map below).</p>
+
+<div class="center">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d124422.20073902117!2d-38.496203619554215!3d-12.959449160922192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x7161625d209fc53%3A0x86ba3d30122b8d76!2sA%C3%A9roport+international+de+Salvador+-+Pra%C3%A7a+Gago+Coutinho%2C+s%2Fn+-+S%C3%A3o+Crist%C3%B3v%C3%A3o%2C+Salvador+-+BA%2C+41520-970!3m2!1d-12.9110977!2d-38.3312411!4m5!1s0x716035d591e29bb%3A0xa45180445486b085!2sHotel+Bahia+Othon+Palace+-+Avenida+Oce%C3%A2nica+-+Ondina%2C+Salvador+-+Bahia%2C+Br%C3%A9sil!3m2!1d-13.010478299999999!2d-38.5135307!5e0!3m2!1sfr!2sfr!4v1437137106865" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+<?php include ("include/footer.php"); ?>
