50a5a384e6d2c4f2af7df1914301fcffbefa61bf
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2015-10-28 17:23:26 -0200

    Fixed issue with older news button: plus and minus sign when expanded/collapsed.

--- web/include/news.php
+++ web/include/news.php
@@ -5,7 +5,8 @@
   <?php add_dateitem("June 16, 2015", 'Call for tutorials online.') ?>
   <?php add_dateitem("June 3, 2015", '"Bahia Othon Palace" selected as the conference venue.') ?>
   <?php add_dateitem("March 31, 2015", 'Web site online.') ?>
-  <li class="newslibtn" data-icon="plus" data-theme="a" onclick="showall();"><a>Older News</a></li>
+  <li class="newslibtn ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-last-child ui-btn-hover-c" data-icon="plus" data-theme="a" onclick="showall();" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-iconpos="right"><div class="ui-btn-inner ui-li"><a class="ui-link-inherit">Older News</a><span class="ui-icon ui-btn-icon-right ui-icon-plus ui-icon-shadow">&nbsp;</span></div></li>
+  <!-- li class="newslibtn" data-icon="plus" data-theme="a" onclick="showall();"><a>Older News</a></li -->
 </ul>
 
 <script>
