715e518f891a9ea2637818da02c2a1c86412fb3e
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-12-29 18:28:30 +0100

    added S. Gorisnky and fixed affiliation consistency

--- web/pc.php
+++ web/pc.php
@@ -16,21 +16,22 @@
     add_listitem("Mohammad Alizadeh", "MIT, USA", "https://people.csail.mit.edu/alizadeh/");
     add_listitem("Katerina Argyraki", "EPFL, Switzerland", "http://people.epfl.ch/katerina.argyraki");
     add_listitem("Hari Balakrishnan", "MIT, USA", "http://nms.csail.mit.edu/~hari/");
-    add_listitem("Hitesh Ballani", "Microsoft Research Cambridge, UK", "http://research.microsoft.com/en-us/um/people/hiballan/");
+    add_listitem("Hitesh Ballani", "Microsoft, UK", "http://research.microsoft.com/en-us/um/people/hiballan/");
     add_listitem("Sujata Banerjee", "HP Labs, USA", "http://www.labs.hpe.com/people/sujata_banerjee/");
     add_listitem("Suman Banerjee", "University of Wisconsin Madison, USA", "http://pages.cs.wisc.edu/~suman/");
     add_listitem("Theo Benson", "Duke University, USA", "https://users.cs.duke.edu/~tbenson/");
-    add_listitem("Ranjita Bhagwan", "Microsoft Research, India", "http://research.microsoft.com/en-us/people/bhagwan/");
+    add_listitem("Ranjita Bhagwan", "Microsoft, India", "http://research.microsoft.com/en-us/people/bhagwan/");
     add_listitem("Fabi&aacute;n Bustamante", "Northwestern University, USA", "http://www.cs.northwestern.edu/~fabianb/");
-    add_listitem("Nandita Dukkipati", "Google Research, USA", "http://research.google.com/pubs/author39115.html");
+    add_listitem("Nandita Dukkipati", "Google, USA", "http://research.google.com/pubs/author39115.html");
     add_listitem("Nick Feamster", "Princeton University, USA", "http://www.cs.princeton.edu/~feamster/");
     add_listitem("Nate Foster", "Cornell University, USA", "http://www.cs.cornell.edu/~jnfoster/");
     add_listitem("Deepak Ganesan", "University of Massachusetts Amherst, USA", "https://people.cs.umass.edu/~dganesan/");
     add_listitem("Yashar Ganjali", "University of Toronto, Canada", "http://www.cs.toronto.edu/~yganjali/");
     add_listitem("Phillipa Gill", "Stony Broook University, USA", "http://www3.cs.stonybrook.edu/~phillipa/");
+    add_listitem("Sergey Gorisnky", "IMDEA Networks Institute, Spain", "http://people.networks.imdea.org/~sergey_gorinsky/");
     add_listitem("Ramesh Govindan", "University of Southern California, USA", "http://sruti.usc.edu/");
     add_listitem("Mark Handley", "University College London, UK", "http://www0.cs.ucl.ac.uk/staff/M.Handley/");
-    add_listitem("Srikanth Kandula", "Microsoft Research Redmond, USA", "http://research.microsoft.com/en-us/um/people/srikanth/home.html");
+    add_listitem("Srikanth Kandula", "Microsoft, USA", "http://research.microsoft.com/en-us/um/people/srikanth/home.html");
     add_listitem("Brad Karp", "University College London, UK", "http://www0.cs.ucl.ac.uk/staff/b.karp/");
     add_listitem("Ethan Katz-Bassett", "University of Southern California, USA", "http://www-bcf.usc.edu/~katzbass/");
     add_listitem("Teemu Koponen", "VMWare, USA", "http://www.vmware.com/");
@@ -38,14 +39,14 @@
     add_listitem("Alex Liu", "Michigan State University, USA", "http://www.cse.msu.edu/~alexliu/");
     add_listitem("Harsha Madhyastha", "University of Michigan, USA", "http://web.eecs.umich.edu/~harshavm/");
     add_listitem("Bruce Maggs", "Duke University, USA", "https://users.cs.duke.edu/~bmm/");
-    add_listitem("Ratul Mahajan", "Microsoft Research Redmond, USA", "http://research.microsoft.com/en-us/um/people/ratul/");
+    add_listitem("Ratul Mahajan", "Microsoft, USA", "http://research.microsoft.com/en-us/um/people/ratul/");
     add_listitem("Vishal Misra", "Columbia University, USA", "http://www.cs.columbia.edu/~misra/");
     add_listitem("Michael Mitzenmacher", "Harvard University, USA", "http://www.eecs.harvard.edu/~michaelm/");
-    add_listitem("Jeff Mogul", "Google Research, USA", "http://research.google.com/pubs/JeffreyMogul.html");
+    add_listitem("Jeff Mogul", "Google, USA", "http://research.google.com/pubs/JeffreyMogul.html");
     add_listitem("Andrew Moore", "University of Cambridge, UK", "http://www.cl.cam.ac.uk/~awm22/");
-    add_listitem("Radhika Mysore", "Google Research, USA", "http://research.google.com/pubs/RadhikaNiranjanMysore.html");
+    add_listitem("Radhika Mysore", "Google, USA", "http://research.google.com/pubs/RadhikaNiranjanMysore.html");
     add_listitem("Dave Oran", "Cisco, USA", "http://www.cisco.com/");
-    add_listitem("Venkat Padmanabhan", "Microsoft Research, India", "http://research.microsoft.com/en-us/people/padmanab/");
+    add_listitem("Venkat Padmanabhan", "Microsoft, India", "http://research.microsoft.com/en-us/people/padmanab/");
     add_listitem("George Papen", "UC San Diego, USA", "http://fleece.ucsd.edu/~gpapen/");
     add_listitem("KyoungSoo Park", "KAIST, Korea", "http://www.ndsl.kaist.edu/~kyoungsoo/");
     add_listitem("Adrian Perrig", "ETH Zurich, Switzerland", "http://www.netsec.ethz.ch/people/aperrig/");
@@ -64,7 +65,7 @@
     add_listitem("Minlan Yu", "University of Southern California, USA", "http://www-bcf.usc.edu/~minlanyu/");
     add_listitem("Matei Zaharia", "MIT, USA", "https://people.csail.mit.edu/matei/");
     add_listitem("Ellen Zegura", "Georgia Tech, USA", "http://www.cc.gatech.edu/fac/Ellen.Zegura/");
-    add_listitem("Ming Zhang", "Microsoft Research Redmond, USA", "http://research.microsoft.com/en-us/people/mzh/");
+    add_listitem("Ming Zhang", "Microsoft, USA", "http://research.microsoft.com/en-us/people/mzh/");
   
   ?>
 </ul>
