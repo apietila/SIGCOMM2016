4c943e7332b8b3f2a718b31ea6748ec568786f44
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-02-08 14:30:22 -0200

    228 paper submissions! yay!!

--- web/include/news.php
+++ web/include/news.php
@@ -1,4 +1,5 @@
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
+  <?php add_dateitem("February 8, 2016", "This year SIGCOMM had a total of 228 paper submissions!") ?>
   <?php add_dateitem("January 23, 2016", "The  <a href=\"postersdemos.php\">Call for Posters &amp; Demos</a> is online") ?>
   <?php add_dateitem("January 5, 2016", "SIGCOMM 2016 <a href=\"submission.php\">paper submission instructions</a> are online") ?>
   <?php add_dateitem("January 5, 2016", "Workshop dates announced") ?>
