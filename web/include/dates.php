b8a97267d0864bc75f777299ce3d45c8aa5f3e0a
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-02-23 20:27:45 -0300

    deadline for tutorial has passed

--- web/include/dates.php
+++ web/include/dates.php
@@ -2,7 +2,7 @@
   <?php add_dateitem("<s>November 23, 2015</s>", "<s>Workshop proposals due</s>"); ?>
   <?php add_dateitem("<s>January 22, 2016</s>", "<s>Main conference paper titles and abstracts due</s>"); ?>
   <?php add_dateitem("<s>January 29, 2016</s>", "<s>Main conference complete paper submissions due</s>"); ?>
-  <?php add_dateitem("February 22, 2016", "<a href=\"cft.php\">Tutorial proposals due</a>"); ?>
+  <?php add_dateitem("<s>February 22, 2016</s>", "<s>Tutorial proposals due</s>"); ?>
   <?php add_dateitem("May 9, 2016", "<a href=\"postersdemos.php\">Posters &amp; Demos submissions due</a>"); ?>
   <?php add_dateitem('August 22-26, 2016', 'SIGCOMM 2016 Main Technical Program'); ?>
 </ul>
