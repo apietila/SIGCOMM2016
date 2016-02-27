7545fe153280818802bd97e74d478c2b562baca1
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-02-27 17:01:49 -0300

    topic preview chairs

--- web/organization.php
+++ web/organization.php
@@ -10,18 +10,18 @@
      add_listitem("Marinho P Barcellos", "UFRGS, Brazil", "http://www.inf.ufrgs.br/~marinho/");
      add_listitem("Jon Crowcroft", "University of Cambridge, UK", "http://www.cl.cam.ac.uk/~jac22/");
   ?>
-	
+
   <li data-role="list-divider">Local Arrangements Chairs</li>
   <?php
      add_listitem("Fab&#237;ola Greve", "UFBA, Brazil", "http://homes.dcc.ufba.br/~fabiola/");
      add_listitem("Leobino Sampaio", "UFBA, Brazil", "http://homes.dcc.ufba.br/~leobino/");
   ?>
-	
-  <li data-role="list-divider">Workshop Chairs</li>
+
+  <li data-role="list-divider">Demo Chairs</li>
   <?php
-     add_listitem("Jussara Almeida","UFMG, Brazil", "http://homepages.dcc.ufmg.br/~jussara/");
-     add_listitem("Lars Eggert", "Netapp, Germany", "https://eggert.org/");
-   ?>
+     add_listitem("Pan Hui", "Hong Kong U. of Science and Technology, Hong Kong", "https://www.cse.ust.hk/~panhui/");
+     add_listitem("Christian E Rothenberg", "UNICAMP, Brazil", "http://www.dca.fee.unicamp.br/~chesteve/");
+  ?>
 
   <li data-role="list-divider">Poster Chairs</li>
   <?php
@@ -29,15 +29,10 @@
      add_listitem("Daniel R Figueiredo", "UFRJ, Brazil", "http://www.land.ufrj.br/~daniel/");
   ?>
   
-  <li data-role="list-divider">Demo Chairs</li>
-  <?php
-     add_listitem("Pan Hui", "Hong Kong U. of Science and Technology, Hong Kong", "https://www.cse.ust.hk/~panhui/");
-     add_listitem("Christian E Rothenberg", "UNICAMP, Brazil", "http://www.dca.fee.unicamp.br/~chesteve/");
-  ?>
-	
-  <li data-role="list-divider">Tutorial Chair</li>
-  <?php
-     add_listitem("Raj Jain", "Washington University in St. Louis, USA", "http://www.cse.wustl.edu/~jain/");
+  <li data-role="list-divider">Publication Chairs</li>
+   <?php
+     add_listitem("Aakanksha Chowdhery", "Princeton University, USA", "http://www.princeton.edu/~ac23/");
+     add_listitem("Matteo Varvello", "Telefonica, Spain", "http://www.tid.es/research/researchers/matteo-varvello");
    ?>
 
   <li data-role="list-divider">Publicity Chairs</li>
@@ -46,16 +41,17 @@
      add_listitem("Hamed Haddadi", "Queen Mary U. of London, UK", "http://www.eecs.qmul.ac.uk/~hamed/");
    ?>
 
-  <li data-role="list-divider">Publication Chairs</li>
-   <?php
-     add_listitem("Aakanksha Chowdhery", "Princeton University, USA", "http://www.princeton.edu/~ac23/");
-     add_listitem("Matteo Varvello", "Telefonica, Spain", "http://www.tid.es/research/researchers/matteo-varvello");
-   ?>
-
   <li data-role="list-divider">Registration Chair</li>
   <?php
      add_listitem("Sharon Goldberg", "Boston University, USA", "http://www.cs.bu.edu/~goldbe/");
    ?>
+
+  <li data-role="list-divider">Topic Preview Chairs</li>
+  <?php
+    add_listitem("&Iacute;talo Cunha", "UFMG, Brazil", "http://homepages.dcc.ufmg.br/~cunha/");
+    add_listitem("Ethan Katz-Bassett", "University of Southern California, USA", "http://www-bcf.usc.edu/~katzbass/");
+    add_listitem("Maria Kazandjieva", "Netflix, USA", "");
+  ?>
 
   <li data-role="list-divider">Travel Grant Chairs</li>
   <?php
@@ -70,11 +66,22 @@
      add_listitem("Christophe Diot", "Safran Analytics, France", "");
   ?>
  
+  <li data-role="list-divider">Tutorial Chair</li>
+  <?php
+     add_listitem("Raj Jain", "Washington University in St. Louis, USA", "http://www.cse.wustl.edu/~jain/");
+   ?>
+
   <li data-role="list-divider">Web Chairs</li>
   <?php
      add_listitem("Weverton Cordeiro", "UFRGS, Brazil", "http://www.inf.ufrgs.br/~wlccordeiro/");
      add_listitem("Anna-Kaisa Pietilainen", "INRIA, France", "https://muse.inria.fr/~apietila/");
   ?>
+
+  <li data-role="list-divider">Workshop Chairs</li>
+  <?php
+     add_listitem("Jussara Almeida","UFMG, Brazil", "http://homepages.dcc.ufmg.br/~jussara/");
+     add_listitem("Lars Eggert", "Netapp, Germany", "https://eggert.org/");
+   ?>
 
   <li data-role="list-divider">Conference Coordinator</li>
   <?php
