adc230cff2532dfe4152e67e5822768b14737eb6
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-03-10 12:47:55 -0300

    gaia deadline extension

--- web/gaia.php
+++ web/gaia.php
@@ -11,8 +11,8 @@
 
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
     <?php add_dateitem("December 15, 2015", "Call for papers published"); ?>
-    <?php add_dateitem("March 11, 2016", "Paper registration deadline"); ?>
-    <?php add_dateitem("March 18, 2016", "Paper submission deadline"); ?>
+    <?php add_dateitem("<s>March 11</s> March 15, 2016", "Paper registration deadline <font color=\"#ff0000\"><b>(extended)</b></font>"); ?>
+    <?php add_dateitem("<s>March 18</s> March 29, 2016", "Paper submission deadline <font color=\"#ff0000\"><b>(extended)</b></font>"); ?>
     <?php add_dateitem("April 29, 2016", "Paper acceptance notification"); ?>
     <?php add_dateitem("Late May, 2016", "Camera-ready deadline"); ?>
     <?php // add_dateitem("June, 2016", "Workshop program online"); ?>
