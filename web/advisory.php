1c726c9c9383e1c3c4bcc228b512d4b187d09420
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-03-23 16:43:41 -0300

    advice for travellers -- florianopolis

--- web/advisory.php
+++ web/advisory.php
@@ -7,14 +7,32 @@
 
 <?php include("include/reloc.php"); ?>
 
+<h2>Florian&oacute;polis</h2>
+
+<p><b>Florian&oacute;polis (FLN)</b> is located in a region with fewer incidence of Aedes Aegypti (mosquitoes found to be the primary vector for spreading Zika virus), as shown in the map below. Also, <a href="http://www.climatempo.com.br/climatologia/377/florianopolis-sc">expected average temperatures for the period of SIGCOMM&#39;16</a> are 21&#186;C (70&#186;F) max. and 14&#186;C (57&#186;F) min. During this period (winter time in South Brazil), <a href="http://www.cdc.gov/dengue/resources/30Jan2012/aegyptifactsheet.pdf">Aegypti density eventually decreases</a> (given its inability to survive under lower temperatures), thus reducing the occurrence of related diseases. The combination of these factors (lower mosquito incidence in the region, plus colder temperatures) makes Florian&oacute;polis a safer region with regard to Zika.</p>
+
+<div align="center">
+  <a href="images/resources/elife-08347-fig1-v3-br_overlay_fln.jpg" rel="external"><img src="images/resources/elife-08347-fig1-v3-br_overlay_fln.jpg" alt="Image: Global map of the predicted distribution of Ae. aegypti (adapted from elifesciences.org)" width="90%"></img></a>
+  <p>Global map of the predicted distribution of Ae. aegypti (source: <a href="http://dx.doi.org/10.7554/eLife.08347.004">elifesciences.org</a>, adapted)<br/>for a larger version, please click on the map</p>
+</div>
+
+<p>Related resources:
+  <ul>
+    <li><a href="http://www.cdc.gov/dengue/resources/30Jan2012/aegyptifactsheet.pdf">US Government CDC Ae. Aegypti fact sheet</a></li>
+    <li><a href="http://www.climatempo.com.br/climatologia/377/florianopolis-sc">Average max. and min. monthly temperatures at Florian&oacute;polis, Brazil</a></li>
+    <li><a href="http://dx.doi.org/10.7554/eLife.08347.004">Global predicted distribution of Ae. Aegypti (eLife Sciences Journal, Jun. 2015)</a></li>
+    <li><a href="http://dx.doi.org/10.7554/eLife.08347.008">Distribution of the occurrence database for Ae. aegypti (A) and Ae. albopictus (B) plotted on the prediction surface (eLife Sciences Journal, Jun. 2015)</a></li>
+  </ul>
+</p>
+
 <h2>Zika Virus</h2>
 
-<p>SIGCOMM'16 organization committee is following closely the latest news and recommendations by the World Health Organization (WHO) and the Brazilian Government concerning the Zika virus alert. We will maintain updated travel advice information on this page.</p>
+<p>SIGCOMM&#39;16 organization committee is following closely the latest news and recommendations by the World Health Organization (WHO) and the Brazilian Government concerning the Zika virus alert. We will maintain updated travel advice information on this page.</p>
 
 <p>For the moment, we refer to the following pages for further information about the alert:
 
 <ul>
-    <li><a href="http://www.who.int/mediacentre/factsheets/zika/en/">WHO's information page on the Zika Virus</a></li>
+    <li><a href="http://www.who.int/mediacentre/factsheets/zika/en/">WHO&#39;s information page on the Zika Virus</a></li>
     <li><a href="http://wwwnc.cdc.gov/travel/page/zika-travel-information">US Government travel information pages</a></li>
     <li><a href="http://agenciabrasil.ebc.com.br/en/geral/noticia/2016-01/brazil-launches-clean-campaign-against-aedes-aegypti">News about the Brazilian Government actions [29/01/2016]</a></li>
 </ul>
