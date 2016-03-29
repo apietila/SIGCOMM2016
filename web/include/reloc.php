bbe3535875306d17c5174a3cc24452740464239c
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2016-03-29 12:52:52 +0200

    remove reloc alert box

--- web/include/reloc.php
+++ web/include/reloc.php
@@ -1,6 +1,10 @@
+<?php  
+/*
 <p class="highlight">
     <b>!! IMPORTANT !!</b><br/><br/>
     ACM SIGCOMM 2016 is going to be relocated to Florian&oacute;polis (South of Brazil) 
     to minimize the risk of Zika virus infections.<br/><br/> 
     Updated travel information will be available soon! 
 </p>
+*/
+?>
