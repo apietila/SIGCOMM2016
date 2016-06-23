6f316ce893964d98a8937d31100e1cf14653188a
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-06-23 18:41:58 -0300

    more on tourism.

--- web/tourism.php
+++ web/tourism.php
@@ -5,9 +5,11 @@
 
 <h1>Tourism</h1>
 
-<p>Below you may find some suggestions of tourism destinations for your visit. You may find information on several other options at <a href="http://www.visitbrasil.com/en/">Visit Brasil website</a>. In case you need some specific tourism information, please contact Mr. Rodrigo Mansilha (<a href="rbmansilha@inf.ufrgs.br">rbmansilha@inf.ufrgs.br</a>).
+<p>Below you may find some suggestions of tourism destinations for your visit. Information on several other destinations can be found at <a href="http://www.visitbrasil.com/en/">Visit Brasil website</a>. In case you need any clarification on the information below, please contact Mr. Rodrigo Mansilha (<a href="rbmansilha@inf.ufrgs.br">rbmansilha@inf.ufrgs.br</a>).
 
-<h2>Fernando de Noronha Archipelago</h2>
+<h2>Fernando de Noronha</h2>
+
+<p><b>Distance to Florian&oacute;polis</b>: 3,133 Km.</p>
 
 <div align="center">
   <a href="https://en.wikipedia.org/wiki/Fernando_de_Noronha#/media/File:EDUARDO_MURUCI_-_PRAIA_DO_CACHORRO.jpg"><img src="images/tourism/noronha/praia-cachorro.jpg" alt="https://en.wikipedia.org/wiki/Fernando_de_Noronha#/media/File:EDUARDO_MURUCI_-_PRAIA_DO_CACHORRO.jpg" width="625px"></img></a><br/>
@@ -24,11 +26,11 @@
 
 <div align="right"><p>(Source: <a href="https://en.wikipedia.org/wiki/Fernando_de_Noronha">Wikimedia</a>)</p></div>
 
-<p>The archipelago of Fernando de Noronha and Atol das Rocas are the only points in the South Atlantic with several species of permanent and migratory birds. The natural pools reveal an exceptional diversity of fish. Fernando de Noronha is also home to the largest concentration of spinner dolphins on the planet. It is located 354 km (220 mi) offshore from the Brazilian coast, and the main island has an area of 18.4 square kilometres (7.1 sq mi), and a population estimated at 2,718 (2012). In 2001, UNESCO designated it as a World Heritage Site, given the island&#39;s importance as a feeding ground for several species, including tuna, billfish, cetaceans, sharks, and marine turtles, its high local population of spinner dolphins, and the goal of protecting endangered species such as the hawksbill sea turtle and other birds.</p>
+<p>The archipelago of Fernando de Noronha and Atol das Rocas are the only points in the South Atlantic with several species of permanent and migratory birds. The natural pools reveal an exceptional diversity of fish. Fernando de Noronha is also home to the largest concentration of spinner dolphins on the planet. It is located 354 km (220 mi) offshore from the Brazilian coast, and the main island has an area of 18.4 square kilometers (7.1 sq mi), and a population estimated at 2,718 (2012). In 2001, UNESCO designated it as a <a href="http://whc.unesco.org/en/list/1000">World Heritage Site</a>, given the island&#39;s importance as a feeding ground for several species, including tuna, billfish, cetaceans, sharks, and marine turtles, its high local population of spinner dolphins, and the goal of protecting endangered species such as the hawksbill sea turtle and other birds.</p>
 
 <p>The beaches of Fernando de Noronha are promoted for tourism and recreational diving. The most popular ones include Ba&iacute;a do Sancho, Pig Bay, Dolphins Bay, Sueste Bay and Praia do Le&atilde;o. Due to the South Equatorial Current that pushes warm water from Africa to the island, diving to depths of 30 to 40 meters does not require a wetsuit. The visibility underwater can reach up to 50 meters.</p>
 
-<p><b>How to Arrive:</b> Fernado de Noronha is served by Carlos Wilson Regional Airport (IATA: FEN, ICAO: SBFN). There are direct flights from Recife Gilberto Freyre Int&#39; Airport (IATA: REC, ICAO: SBRF) and Natal Alu&iacute;zio Alves Int&#39;l Airport (IATA: NAT, ICAO: SBSG). Round trip tickets from Florian&oacute;polis (FLN) to Fernando de Noronha may range from R$ 2,500 to R$ 3,000, through airline companies <a href="http://www.voeazul.com.br/en/home">Azul Airlines</a> and <a href="https://www.voegol.com.br/en-us/Paginas/Default.aspx">Gol Airlines</a>.</p>
+<p><b>How to Arrive:</b> Fernando de Noronha is served by Carlos Wilson Regional Airport (IATA: FEN, ICAO: SBFN). Round trip tickets from Florian&oacute;polis (FLN) to Fernando de Noronha (FEN) may range from R$ 2,500 to R$ 3,000, through airline companies <a href="http://www.voeazul.com.br/en/home">Azul Airlines</a> and <a href="https://www.voegol.com.br/en-us/Paginas/Default.aspx">Gol Airlines</a>. Cheaper flights may be found arriving or departing from one of Brazilian major airport hubs, such as S&atilde;o Paulo Guarulhos (GRU) and Rio de Janeiro Gale&atilde;o (GIG).</p>
 
 <p><b>Accommodation:</b> Prices may range from R$ 500 to R$ 2,000 per night of stay, and reservations may be done through <a href="http://www.hotels.com/">Hotels.com</a> or <a href="http://www.booking.com/">Booking.com</a>. Please observe that admission to Fernando de Noronha requires the payment of a <a href="http://www.prontotecnologia.com.br/noronha2/turPreservacao.php">Preservation Tax</a>. The total amount due varies non-linearly with the intended duration of the stay.</p>
 
@@ -41,7 +43,39 @@ You may find further information about Fernando de Noronha in the following link
 </ul>
 </p>
 
+<h2>Igua&ccedil;u Falls</h2>
+
+<p><b>Distance to Florian&oacute;polis</b>: 650 Km.</p>
+
+<div align="center">
+  <a href="https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguazu_D%C3%A9cembre_2007_-_Panorama_7.jpg"><img src="images/tourism/foz-iguacu/iguacu-panorama.jpg" alt="Image: https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguazu_D%C3%A9cembre_2007_-_Panorama_7.jpg" width="625px"></img></a><br/>
+  <a href="https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguacu-004.jpg"><img src="images/tourism/foz-iguacu/iguacu-falls.jpg" class="img" alt="Image: https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguacu-004.jpg" width="400px" height="200px" ></img></a>
+  <a href="https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguazu_D%C3%A9cembre_2007_-_Panorama_3.jpg"><img src="images/tourism/foz-iguacu/iguacu-garganta-diablo.jpg" class="img" alt="Image: https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Iguazu_D%C3%A9cembre_2007_-_Panorama_3.jpg" width="400px" height="200px" ></img></a>
+  <a href="https://en.wikipedia.org/wiki/Foz_do_Igua%C3%A7u#/media/File:Foz_de_Igua%C3%A7u_27_Panorama_Nov_2005.jpg"><img src="images/tourism/foz-iguacu/foz-panorama-2.jpg" class="img" alt="Image: https://en.wikipedia.org/wiki/Foz_do_Igua%C3%A7u#/media/File:Foz_de_Igua%C3%A7u_27_Panorama_Nov_2005.jpg" width="400px" height="200px" ></img></a>
+  <a href="https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Cataratas_do_Igua%C3%A7u_.jpg"><img src="images/tourism/foz-iguacu/iguacu-trail.jpg" class="img" alt="Image: https://en.wikipedia.org/wiki/Iguazu_Falls#/media/File:Cataratas_do_Igua%C3%A7u_.jpg" width="400px" height="200px" ></img></a>
+</div>
+<div align="right"><p>(Source: <a href="https://en.wikipedia.org/wiki/Iguazu_Falls">Wikimedia</a>)</p></div>
+
+<p>Spanning 2.7 kilometers, Igua&ccedil;u Falls are a complex of over than 250 waterfalls located in the Brazilian state of Paran&aacute; and the Iguazu River on the border of the Argentine province of Misiones. The falls may be reached from two main towns, with one on either side of the falls: Foz do Igua&ccedil;u in Brazil and Puerto Iguaz&uacute; in Argentina. The falls are shared by the Igua&ccedil;u National Park (Brazil) and Iguaz&uacute; National Park (Argentina). The two parks were designated UNESCO World Heritage Sites.</p>
+
+<p>The water flow at Foz do Igua&ccedil;u can be equal to <a href="http://edition.cnn.com/2015/03/24/travel/iguazu-falls-bucket-list/">three times that of Niagara Falls, and significantly wider than Victoria Falls</a>. A visit to the Igua&ccedil;u Falls is an experience of immersion in nature through walking trails in the woods and the proximity of the immensity of the waterfalls in the Iguazu River. Located within the Igua&ccedil;u National Park, the surroundings have a qualified structure for receiving tourists, with a visitors center, parking, hotel, restaurant, transfer buses, additional sightseeing tours, shops, food court, and a hiking trail to access the falls. Helicopter rides for a privileged view of the waterfalls can be also hired on site.</p>
+
+<p>Closer to the Igua&ccedil;u Falls you may also reach the <a href="https://en.wikipedia.org/wiki/Itaipu_Dam">Itaipu Dam (Itaipu Hydroelectric Plant)</a>. It is recognized as one of the greatest works of modern engineering, largest hydroelectric plant in power production, and until recently the largest in size. It provides an organized tour service that includes several visiting options in the areas of engineering, technology, nature, and local history.</p>
+
+<p><b>How to Arrive:</b> Foz do Igua&ccedil;u is served by Foz do Igua&ccedil;u - Cataratas International Airport (IATA: IGU, ICAO: SBFI). Round trip tickets from Florian&oacute;polis (FLN) to Foz do Igua&ccedil;u (IGU) range from R$ 550 to R$ 750, through airline companies Azul, Gol, and TAM.</p>
+
+<p><b>Accommodation:</b> Prices may range from R$ 100 to R$ 400 per night of stay, and reservations may be done through <a href="http://www.hotels.com/">Hotels.com</a> or <a href="http://www.booking.com/">Booking.com</a>.</p>
+
+<p>You may find further information about Foz do Igua&ccedil;u in the following links:
+<ul>
+  <li><a href="http://www.visitefoz.com.br/en">Oficial Tourism Website for Foz do Igua&ccedil;u</a></li>
+  <li><a href="https://en.wikipedia.org/wiki/Iguazu_Falls">Foz do Igua&ccedil;u</a> on Wikipedia</li>
+  <li><a href="http://wikitravel.org/en/Foz_do_Igua%C3%A7u">Foz do Igua&ccedil;u</a> on Wikitravel</li>
+</ul>
+
 <h2>Bonito, Mato Grosso do Sul</h2>
+
+<p><b>Distance to Florian&oacute;polis</b>: 1,078 Km.</p>
 
 <div align="center">
   <a href="https://pt.wikipedia.org/wiki/Bonito_%28Mato_Grosso_do_Sul%29#/media/File:Abismo_Anhumas,_Bonito,_MS.JPG"><img src="images/tourism/bonito/abismo-anhumas.jpg" alt="https://pt.wikipedia.org/wiki/Bonito_%28Mato_Grosso_do_Sul%29#/media/File:Abismo_Anhumas,_Bonito,_MS.JPG" width="625px"></img></a><br/>
@@ -64,11 +98,11 @@ You may find further information about Fernando de Noronha in the following link
 <iframe width="320" height="240" src="https://www.youtube.com/embed/wj-jrzP_AS4?wmode=opaque&showinfo=0&amp;autoplay=0&amp;controls=1&amp;modestbranding=1&amp;vq=&amp;rel=0" frameborder="0" allowfullscreen></iframe>
 </center>
 
-<p>Bonito is widely regarded as Brazil's capital of ecotourism. There are several tours options available in the region, including Abismo Anhumas (Anhumas Abism), Rio da Prata (Silver River), Aquário Natural (Natural Aquarium), Circuito Arvorismo (The trees circuit), and Gruta do Lago Azul (Blue Lake Cave). Some tours must be booked through a local, official tourist agency and require an accredited guide.</p>
+<p>Bonito is widely regarded as Brazil's capital of ecotourism. There are several tour options available in the region, including Abismo Anhumas (Anhumas Abism), Rio da Prata (Silver River), Aqu&aacute;rio Natural (Natural Aquarium), Circuito Arvorismo (The trees circuit), and Gruta do Lago Azul (Blue Lake Cave). Some tours must be booked through a local, official tourist agency and require an accredited guide.</p>
 
-<p><b>How to Arrive:</b> Bonito is served by Bonito Regional Airport (IATA: BYO, ICAO: SBDB), which can be directly reached from Campinas Int&#39;l Airport (VCP) through <a href="http://www.voeazul.com.br/en/home">Azul Airlines</a>. The nearest airport with connecting flights from Hercilio Luz (FLN) is Campo Grande Int&#39;l Airport (CGR). Round trip tickets may range from R$ 800 to R$ 1,500.</p>
+<p><b>How to Arrive:</b> Bonito is served by Bonito Regional Airport (IATA: BYO, ICAO: SBDB), which can be directly reached from Campinas Int&#39;l Airport (VCP) through <a href="http://www.voeazul.com.br/en/home">Azul Airlines</a>. The airport nearest to Bonito with connecting flights from Florian&oacute;polis Hercilio Luz (FLN) is Campo Grande Int&#39;l Airport (IATA: CGR, ICAO: SBCG). Round trip tickets may range from R$ 800 to R$ 1,500.</p>
 
-<p>The distance between Campo Grande and Bonito can be 260 km (taking highways BR 060 and BR 419) or 330 km (taking highways BR 262 and BR 419). From Campo Grande, there are executive buses (van) departing to Bonito at 09:30 / 14:30 / 18:00 / 23:00, and returning at 07:30 / 10:00 / 12:30 / 18:30 (approximately 4 hours trip). The transfer can be purchased for prices starting at R$ 100.00. Another option is taking regular buses departing from Campo Grande Bus Station. Car rental service is also available at Campo Grande Int&#39;l Airport, for your convenience. For further information, please refer to <a href="http://www.portalbonito.com.br/a-cidade/como-chegar">Portal Bonito</a> website.</p>
+<p>The distance between Campo Grande and Bonito can be 260 km (taking highways BR 060 and BR 419) or 330 km (taking highways BR 262 and BR 419). From Campo Grande, there are executive buses (van type) departing to Bonito at 09:30 / 14:30 / 18:00 / 23:00, and returning at 07:30 / 10:00 / 12:30 / 18:30 (approximately 4 hours trip). The transfer can be purchased for prices starting at R$ 100.00. Another option is taking regular buses departing from Campo Grande Central Bus Station. Car rental service is also available at Campo Grande Int&#39;l Airport, for your convenience. For further information, please refer to <a href="http://www.portalbonito.com.br/a-cidade/como-chegar">Portal Bonito</a> website.</p>
 
 <p><b>Accommodation:</b> Prices may range from R$ 150 to R$ 550 per night of stay, and reservations may be done through <a href="http://www.hotels.com/">Hotels.com</a> or <a href="http://www.booking.com/">Booking.com</a>.</p>
 
@@ -93,8 +127,14 @@ You may find further information about Bonito in the following links:
 </p>
 
 <div align="center">
-  <img src="images/tourism/florianopolis/800px-Armacao_florianopolis_brazil.jpg" class="img" alt="Image: http://wikitravel.org/en/File:Armacao_florianopolis_brazil.jpg" width="400px"></img>
-  <img src="images/tourism/florianopolis/Lagoa_da_conceicao1.jpg" class="img" alt="Image: http://wikitravel.org/en/File:Lagoa_da_conceicao1.jpg" width="400px"></img>
+  <a href="http://wikitravel.org/en/File:Armacao_florianopolis_brazil.jpg"><img src="images/tourism/florianopolis/800px-Armacao_florianopolis_brazil.jpg" class="img" alt="Image: http://wikitravel.org/en/File:Armacao_florianopolis_brazil.jpg" width="400px" height="200px" ></img></a>
+  <a href="http://wikitravel.org/en/File:Lagoa_da_conceicao1.jpg"><img src="images/tourism/florianopolis/Lagoa_da_conceicao1.jpg" class="img" alt="Image: http://wikitravel.org/en/File:Lagoa_da_conceicao1.jpg" width="400px" height="200px" ></img></a>
+</div>
+<div align="right"><p>(Source: <a href="http://wikitravel.org/en/Florian%C3%B3polis">Wikitravel</a>)</p></div>
+
+<div align="center">
+  <a rel="external" href="images/tourism/florianopolis/florianopolis-1.jpg"><img src="images/tourism/florianopolis/florianopolis-1.jpg" class="img" width="400px" height="200px" ></img></a>
+  <a rel="external" href="images/tourism/florianopolis/florianopolis-2.jpg"><img src="images/tourism/florianopolis/florianopolis-2.jpg" class="img" width="400px" height="200px" ></img></a>
 </div>
 
 <p>
