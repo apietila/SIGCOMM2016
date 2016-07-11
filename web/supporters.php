fe9e3db38f9a0099bb50129a0d593819c8d5ff15
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-07-11 17:10:16 -0300

    adding cgi.br and nsf as sponsors, and reorganizing other logos (and sponsor ticker tape)

--- web/supporters.php
+++ web/supporters.php
@@ -5,10 +5,13 @@
 
 <h1>Industrial Supporters</h1>
 
+<!-- when adding sponsor logos here, need also to add them to the sponsor ticker tape - see include/footer.php file -->
+
 <h2>Platinum</h2>
 
 <div class="sponsors">
         <a href="//www.cisco.com/"><img src="images/sponsors/cisco.png" alt="Cisco" style="width: 250px;"/></a>
+        <a href="//www.nsf.gov/"><img src="images/sponsors/nsf.jpg" alt="NSF" style="width: 200px;"/></a>
 </div>
 
 <h2>Gold</h2>
@@ -17,14 +20,15 @@
 	<a href="//www.huawei.com/"><img src="images/sponsors/huawei.png" alt="Huawei" style="width: 130px;"/></a>
 	<a href="//www.ericsson.com/"><img src="images/sponsors/ericsson.png" alt="Ericsson" style="width: 150px;"/></a><br/>
 	<a href="//research.facebook.com/"><img src="images/sponsors/facebook.png" alt="Facebook" style="width: 330px;"/></a>
-	<a href="//www.google.com/"><img src="images/sponsors/google.png" alt="Google" style="width: 230px;"/></a>
+	<a href="//www.google.com/"><img src="images/sponsors/google.png" alt="Google" style="width: 290px;"/></a><br/>
+        <a href="//cgi.br/about/"><img src="images/sponsors/cgi.png" alt="CGI.br" style="width: 160px;"/></a>
 </div>
 
 
 <h2>Silver</h2>
 
 <div class="sponsors">
-	<a href="//www.akamai.com/"><img src="images/sponsors/akamai.png" alt="Akamai" style="width: 210px;"/></a>
+	<a href="//www.akamai.com/"><img src="images/sponsors/akamai.png" alt="Akamai" style="width: 260px;"/></a>
 </div>
 
 
@@ -34,8 +38,8 @@
 	<a href="//www.hp.com/"><img src="images/sponsors/hp.png" alt="Hewlett-Packard" style="width: 80px;"/></a>
 	<a href="//www.netapp.com/"><img src="images/sponsors/netapp.jpeg" alt="NetApp" style="width: 200px;"/></a><br/>
 	<a href="//www.tid.es/"><img src="images/sponsors/telefonica.png" alt="Telef&oacute;nica" style="width: 120px;"/></a>
-	<a href="//www.vmware.com/"><img src="images/sponsors/vmware.jpg" alt="VMWare" style="width: 160px;"/></a>
-	<a href="//www.comcast.com/"><img src="images/sponsors/comcast.jpg" alt="Comcast" style="width: 140px;"/></a>
+	<a href="//www.vmware.com/"><img src="images/sponsors/vmware.jpg" alt="VMWare" style="width: 180px;"/></a>
+	<a href="//www.comcast.com/"><img src="images/sponsors/comcast.jpg" alt="Comcast" style="width: 180px;"/></a>
 </div>
 
 <?php include ("include/footer.php"); ?>
