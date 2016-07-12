d0162bbc94448423297335625777999af1f2a653
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-07-12 11:46:22 -0300

    including news item on early bird deadline (plus some further adjustments on the sponsors page)

--- web/supporters.php
+++ web/supporters.php
@@ -37,7 +37,7 @@
 <div class="sponsors">
 	<a href="//www.hp.com/"><img src="images/sponsors/hp.png" alt="Hewlett-Packard" style="width: 80px;"/></a>
 	<a href="//www.netapp.com/"><img src="images/sponsors/netapp.jpeg" alt="NetApp" style="width: 200px;"/></a><br/>
-	<a href="//www.tid.es/"><img src="images/sponsors/telefonica.png" alt="Telef&oacute;nica" style="width: 120px;"/></a>
+	<a href="//www.tid.es/"><img src="images/sponsors/telefonica.png" alt="Telef&oacute;nica" style="width: 130px;"/></a>
 	<a href="//www.vmware.com/"><img src="images/sponsors/vmware.jpg" alt="VMWare" style="width: 180px;"/></a>
 	<a href="//www.comcast.com/"><img src="images/sponsors/comcast.jpg" alt="Comcast" style="width: 180px;"/></a>
 </div>
