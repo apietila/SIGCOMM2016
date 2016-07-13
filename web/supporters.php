308cb35beefab04daec8933f62935c360b1f9591
Weverton Cordeiro <weverton.cordeiro@inf.ufrgs.br>
2016-07-12 21:10:46 -0300

    reshaping the supporters page to better align logos

--- web/supporters.php
+++ web/supporters.php
@@ -10,36 +10,108 @@
 <h2>Platinum</h2>
 
 <div class="sponsors">
-        <a href="//www.cisco.com/"><img src="images/sponsors/cisco.png" alt="Cisco" style="width: 250px;"/></a>
-        <a href="//www.nsf.gov/"><img src="images/sponsors/nsf.jpg" alt="NSF" style="width: 200px;"/></a>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center" width="50%">
+               <a href="//www.cisco.com/"><img src="images/sponsors/cisco.png" alt="Cisco" style="width: 250px;"/></a>
+            </td>
+            <td align="center"  width="50%">
+               <a href="//www.nsf.gov/"><img src="images/sponsors/nsf.jpg" alt="NSF" style="width: 200px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
 </div>
 
 <h2>Gold</h2>
 
 <div class="sponsors">
-	<a href="//www.huawei.com/"><img src="images/sponsors/huawei.png" alt="Huawei" style="width: 130px;"/></a>
-	<a href="//www.ericsson.com/"><img src="images/sponsors/ericsson.png" alt="Ericsson" style="width: 150px;"/></a><br/>
-	<a href="//research.facebook.com/"><img src="images/sponsors/facebook.png" alt="Facebook" style="width: 330px;"/></a>
-	<a href="//www.google.com/"><img src="images/sponsors/google.png" alt="Google" style="width: 290px;"/></a><br/>
-        <a href="//cgi.br/about/"><img src="images/sponsors/cgi.png" alt="CGI.br" style="width: 160px;"/></a>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center" width="50%">
+               <a href="//www.huawei.com/"><img src="images/sponsors/huawei.png" alt="Huawei" style="width: 130px;"/></a>
+            </td>
+            <td align="center"  width="50%">
+               <a href="//www.ericsson.com/"><img src="images/sponsors/ericsson.png" alt="Ericsson" style="width: 150px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center"  width="50%">
+               <a href="//research.facebook.com/"><img src="images/sponsors/facebook.png" alt="Facebook" style="width: 320px;"/></a>
+            </td>
+            <td align="center"  width="50%">
+               <a href="//www.google.com/"><img src="images/sponsors/google.png" alt="Google" style="width: 290px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center">
+               <a href="//cgi.br/about/"><img src="images/sponsors/cgi.png" alt="CGI.br" style="width: 160px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
 </div>
-
 
 <h2>Silver</h2>
 
 <div class="sponsors">
-	<a href="//www.akamai.com/"><img src="images/sponsors/akamai.png" alt="Akamai" style="width: 260px;"/></a>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center">
+               <a href="//www.akamai.com/"><img src="images/sponsors/akamai.png" alt="Akamai" style="width: 260px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
 </div>
-
 
 <h2>Bronze</h2>
 
 <div class="sponsors">
-	<a href="//www.hp.com/"><img src="images/sponsors/hp.png" alt="Hewlett-Packard" style="width: 80px;"/></a>
-	<a href="//www.netapp.com/"><img src="images/sponsors/netapp.jpeg" alt="NetApp" style="width: 200px;"/></a><br/>
-	<a href="//www.tid.es/"><img src="images/sponsors/telefonica.png" alt="Telef&oacute;nica" style="width: 130px;"/></a>
-	<a href="//www.vmware.com/"><img src="images/sponsors/vmware.jpg" alt="VMWare" style="width: 180px;"/></a>
-	<a href="//www.comcast.com/"><img src="images/sponsors/comcast.jpg" alt="Comcast" style="width: 180px;"/></a>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center"  width="20%">
+               &nbsp;
+            </td>
+            <td align="center" width="30%">
+               <a href="//www.hp.com/"><img src="images/sponsors/hp.png" alt="Hewlett-Packard" style="width: 80px;"/></a>
+            </td>
+            <td align="center"  width="30%">
+               <a href="//www.netapp.com/"><img src="images/sponsors/netapp.jpeg" alt="NetApp" style="width: 200px;"/></a>
+            </td>
+            <td align="center"  width="20%">
+               &nbsp;
+            </td>
+         </tr>
+      </table>
+   </center>
+   <center>
+      <table width="100%" border="0">
+         <tr>
+            <td align="center" width="33%">
+               <a href="//www.tid.es/"><img src="images/sponsors/telefonica.png" alt="Telef&oacute;nica" style="width: 130px;"/></a>
+            </td>
+            <td align="center"  width="34%">
+               <a href="//www.vmware.com/"><img src="images/sponsors/vmware.jpg" alt="VMWare" style="width: 180px;"/></a>
+            </td>
+            <td align="center"  width="33%">
+               <a href="//www.comcast.com/"><img src="images/sponsors/comcast.jpg" alt="Comcast" style="width: 180px;"/></a>
+            </td>
+         </tr>
+      </table>
+   </center>
 </div>
 
 <?php include ("include/footer.php"); ?>
